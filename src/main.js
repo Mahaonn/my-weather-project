a5c7d25019f4fbbf7fdc9a1cbb71416aedbf924a
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-01-25 18:47:29 +0100

    organized the code in styles, added forecast code to main.js

---
+++ src/main.js
@@ -0,0 +1,91 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
+  let currentTimeELement = document.querySelector("#current-time");
+  let currentDate = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-icon");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(windSpeed * 10) / 10;
+  currentTimeELement.innerHTML = formatDate(currentDate);
+  iconElement.innerHTML = `<img src="./images/sunny-night.png "class="weather-icon" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "7b358bb45a2c3obdef533te70adb056a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity(searchInputElement.value);
+}
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+            <div class="weather-forecast-day">
+              <img
+                src="./images/sunny-night.png"
+                width="80"
+                class="weather-forecast-icon"
+              />
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                  <strong>15º</strong><span>/9º</span>
+                </div>
+              </div>
+              <div class="weather-forecast-date">${day}</div>
+            </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kyiv");
+displayForecast();
