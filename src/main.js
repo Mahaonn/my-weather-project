96cb457a9b770940221283296a11c39dfbc9af8f
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-02-06 11:36:59 +0100

    Changed API key

--- src/main.js
+++ src/main.js
@@ -95,7 +95,7 @@ function formatDay(timestamp) {
 }
 
 function getForecast(city) {
-  let apiKey = "7b358bb45a2c3obdef533te70adb056a";
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
